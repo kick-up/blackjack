3426a6212757826ad95a5c002a059f336b13e5eb
Ваше Имя <darhanbek89@gmail.com>
2021-12-13 02:19:49 +0600

    fix

--- card.rb
+++ card.rb
@@ -1,5 +1,4 @@
 class Card
-
   attr_reader :suit, :value, :rank
 
   def self.suits
@@ -23,8 +22,25 @@ class Card
   end
 end
 
-class Bank
+class Deck
+  attr_accessor :deck
 
+  def deck_fill
+    @deck = []
+    Card.suits.each do |suit|
+      Card.ranks.each do |rank, value|
+        @deck << Card.new(rank, suit, value)
+      end
+    end
+    @deck.shuffle!
+  end
+
+  def take_one_card
+    @deck.shift
+  end
+end
+
+class Bank
   attr_accessor :money
 
   def initialize
@@ -48,30 +64,14 @@ class Bank
   end
 end
 
-class Deck
-
-  attr_accessor :deck
-
-  def deck_fill
-    @deck = []
-    Card.suits.each do |suit|
-      Card.ranks.each do |rank, value|
-        @deck << Card.new(rank, suit, value)
-      end
-    end
-    @deck.shuffle!
-  end
-
-  def take_one_card
-    @deck.shift
-  end
-
-end
-
 class Hand 
   attr_accessor :cards
 
   def initialize
+    @cards = []
+  end
+
+  def clear
     @cards = []
   end
 
@@ -90,7 +90,6 @@ class Hand
   def show
     @cards.map(&:to_s)
   end
-
 end
 
 class Player
@@ -98,7 +97,7 @@ class Player
 
   def initialize(name)
     @name = name
-    #@bank = Bank.new
+    @bank = Bank.new
     @hand = Hand.new
     @deck = Deck.new
     @deck.deck_fill
@@ -119,7 +118,7 @@ class Player
   end
 
   def place_a_bet
-     @money -= 10
+    @money -= 10
   end
 
   def show_card
@@ -133,7 +132,6 @@ class Player
   def money?
     @money.zero?
   end
-
 end
 
 class Dealer < Player
@@ -141,78 +139,125 @@ class Dealer < Player
   def initialize(name)
     super
   end
+
+  def dealer_info
+    if @open_card == false
+      "#{name}: Карты: #{"**"} Очки: #{"*"} Остаток денег на счету: #{@money}"
+    else
+      info
+    end
+  end
+
+  def info
+    "#{name}: Карты: #{show_card} Очки:(#{hand.total}) Остаток денег на счету: #{@money}"
+    #{}"#{name}: Карты: #{"**"} Очки: #{"*"} Остаток денег на счету: #{@money}"
+  end
 end
 
-
 class Game
-
  attr_accessor :money, :deck, :dealer, :player
 
  BJ = 21
-
-  def new
-    game_table
-    create_players
-    hand_out_cards
-    loop do 
-      show_list
-      player_action
-      break if @player.open_hand
-
-      dealer_action
-      break if @player.hand.maximum_cards? && @dealer.hand.maximum_cards?
-    end
-    open_hand
-    farther
-  end
 
   def game_table
     @money ||= Bank.new
     @deck ||= Deck.new
     @deck.deck_fill
-    @dealer ||= create_players
-    @player ||= create_players
-    @step = {"1": "Пропустить ход", "2": "Взять карту", "3": "Открыть карту"}
+    @dealer = Dealer.new('Dealer')
   end
 
   def create_players
-    puts "Введите ваше имя"
-    player_name = "Darkhan"
+    player_name = gets.chomp.capitalize!
     @player = Player.new(player_name)
-    dealer_name = "Dealer"
-    @dealer = Dealer.new(dealer_name)
+  end
+
+  def start
+    puts "**************************************"
+    puts "Блэк Джэк"
+    puts "Укажите имя:"
+    create_players
+  end
+
+  def menu
+    puts "**************************************"
+    show_list
+    puts "**************************************"
+    puts "Выберите соответствующий номер:"
+    puts "1. Начать новую игру"
+    puts "2. Пропустить ход"
+    puts "3. Взять карту"
+    puts "4. Показать карты"
+    puts "0. Выход"
+  end
+
+  def choice
+    start
+    loop do
+      menu
+      choice = gets.to_i
+      case choice
+      when 1 then hand_out_cards
+      when 2 then miss
+      when 3 then take_card
+      when 4 then open_hand
+      when 0 then exit
+      end
+    end
   end
 
   def hand_out_cards
     puts "Игра начинается, раздается по две карты"
+    @player.hand.clear
+    @dealer.hand.clear
     @player.take_two_card
     @dealer.take_two_card
     @money.put(20)
     @player.place_a_bet
     @dealer.place_a_bet
+    @dealer.take_one_card if @dealer.hand.total < 17
+    return player_win if @player.hand.total == BJ || dealer.hand.total > BJ
+    return dealer_win if @dealer.hand.total == BJ || player.hand.total > BJ
   end
 
-  def player_action
-    player_step
-    action = gets.chomp.to_sym
-    #......
-    # .....
+  def miss
+    puts "Пропуск хода"
+    @dealer.take_one_card if @dealer.hand.total < 17
+    puts "Диллер берерт одну карту"
+    return player_win if @dealer.hand.total > BJ
+    return dealer_win if @dealer.hand.total == BJ
   end
- 
-  def dealer_action
-    puts "Ход диллера"
-    @dealer.take_one_card if @dealer.total < 17
+
+  def take_card
+    if @dealer.hand.total < 17 && @dealer.hand.maximum_cards?
+      puts "Дилер берет себе одну карту" 
+      @dealer.take_one_card 
+    else
+      puts "Дилер дал вам одну карту"
+      @player.take_one_card && @player.hand.maximum_cards?
+    end
+    return dealer_win if @player.hand.total > BJ
+    return player_win if @player.hand.total == 21
+  end
+
+  def open_hand
+    puts "Открыть карты"
+    @dealer.show_card
+    dealer_total = @dealer.hand.total
+    player_total = @player.hand.total
+    return draw if dealer_total == player_total
+    return draw if dealer_total > BJ && player_total > BJ
+    return dealer_win if player_total > BJ || player_total < dealer_total
+    return player_win if dealer_total > BJ || dealer_total < player_total
   end
 
   def player_win
-    puts "Игрок выграл"
-    @player
-    #.....
+    puts "Вы выйграли"
   end
 
   def dealer_win
-    @dealer
-    #....
+    puts "Дилер выйграл"
+    @open_card = true
+    @dealer.show_card
   end
 
   def draw
@@ -228,21 +273,10 @@ class Game
     end
   end
 
-  def open_hand
-    puts "Открыть карты"
-    @dealer.show_card
-    dealer_total = @dealer.total
-    player_total = @player.total
-    return draw if dealer_total == player_total
-    return draw if dealer_total > BJ && player_total > BJ
-    return dealer_win if player_total > BJ
-
-    player_win
-  end
-
   def show_list
     puts @player.info
-    puts @dealer.info  
+    @open_card = false
+    puts @dealer.info 
     puts @money.info
   end
 
@@ -252,6 +286,8 @@ class Game
       puts "#{index} - #{value}"
     end
   end
-
 end
 
+game = Game.new
+game.game_table
+game.choice
