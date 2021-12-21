c156a39f6ea9a629e843f8bb4c4916d9f50011e0
Ваше Имя <darhanbek89@gmail.com>
2021-12-22 00:50:25 +0600

    fix

--- card.rb
+++ card.rb
@@ -15,7 +15,7 @@ class Card
     @rank = rank.to_sym
     @suit = suit.to_sym
     @value = { "A": 11, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6,
-               "7": 7, "8": 8, "9": 9,  J: 10, Q: 10, K: 10 }
+               "7": 7, "8": 8, "9": 9, T: 10, J: 10, Q: 10, K: 10 }
   end
 
   def ace?
